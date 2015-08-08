dd53a9159d8645de1f76d9d04c1cb58aebc4ef21
Ryan Goodfellow <rgoodfel@isi.edu>
2015-08-07 17:32:15 -0700

    minimal topdl

--- spi/topdl.go
+++ spi/topdl.go
@@ -1,4 +1,46 @@
 package spi
 
-type TopDL struct {
+type Topology struct {
+	Substrates []Substrate
+	Elements   []Element
+}
+
+type Substrate struct {
+	Name     string
+	Capacity Capacity
+	Latency  Latency
+}
+
+type Element interface{}
+
+type Computer struct {
+	Name      string
+	Interface []Interface
+	OSs       []OS
+}
+
+type Interface struct {
+	Name      string
+	Substrate string
+	Capacity  Capacity
+	Latency   Latency
+}
+
+type OS struct {
+	Name    string
+	Version string
+}
+
+type Capacity struct {
+	Rate float64
+	Kind Kind
+}
+
+type Latency struct {
+	Time float64
+	Kind Kind
+}
+
+type Kind struct {
+	Value String //should be either "max" or "average"
 }
